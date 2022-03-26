b9dd7f6bdca6fbf964e61e35cef5981167e923ce
williamfchang <wfchang@usc.edu>
2022-03-26 14:52:41 -0700

    Add initial features to map fragment - building markers that appear given a zoom level - clickable marker info windows

--- app/src/main/java/com/example/covider/MapsFragment.kt
+++ app/src/main/java/com/example/covider/MapsFragment.kt
@@ -3,32 +3,71 @@ package com.example.covider
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
-class MapsFragment : Fragment() {
 
-    private val callback = OnMapReadyCallback { googleMap ->
+class MapsFragment : Fragment(), GoogleMap.OnInfoWindowClickListener {
+
+    // member vars
+    private val buildings = arrayOf(
+        Building("Tutor Campus Center", LatLng(34.020473494587414, -118.2863610986055)),
+        Building("Grace Ford Salvatori Hall", LatLng(34.02145610419585, -118.2879972637911),
+            minZoom = 17.0f),
+        Building("Leavey Library", LatLng(34.02178321140542, -118.28284243164403),
+            isFavorite = true, minZoom = 16.0f)
+    )
+    private var buildingMarkers = ArrayList<Marker>()
+
+
+    private val callback = OnMapReadyCallback { map ->
         /**
          * Manipulates the map once available.
          * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        // Set initial camera settings
+        val uscBounds = LatLngBounds(
+            LatLng(34.016779, -118.293452), // southwest corner
+            LatLng(34.027559, -118.277238) // northeast corner
+        )
+        map.moveCamera(CameraUpdateFactory.newLatLngBounds(uscBounds, 0))
+        map.setLatLngBoundsForCameraTarget(uscBounds)
+        map.setMinZoomPreference(14.0f)
+
+        // Draw markers at each building
+        for (b in buildings) {
+            // get icon color based on favorited
+            val hue = if (b.isFavorite) BitmapDescriptorFactory.HUE_YELLOW
+                        else BitmapDescriptorFactory.HUE_RED
+            val customIcon = BitmapDescriptorFactory.defaultMarker(hue)
+
+            // create marker
+            val marker = map.addMarker(MarkerOptions().position(b.coords)
+                .title(b.name).snippet("Click for more info")
+                .icon(customIcon))
+
+            if (marker != null) {
+                marker.tag = b // set data tag of marker to corresponding building object
+                buildingMarkers.add(marker)
+            }
+        }
+        updateMarkersOnZoom(map) // an initial update for current zoom
+
+        // Set listener to zoom so we can draw markers based on zoom
+        map.setOnCameraMoveListener { updateMarkersOnZoom(map) }
+
+        // Set listener to marker click
+        map.setOnInfoWindowClickListener(this)
     }
 
     override fun onCreateView(
@@ -44,4 +83,20 @@ class MapsFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
+    override fun onInfoWindowClick(marker: Marker) {
+        Toast.makeText(
+            activity, "info window of ${marker.title} clicked",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun updateMarkersOnZoom(map: GoogleMap) {
+        for (marker in buildingMarkers) {
+            marker.isVisible = map.cameraPosition.zoom >= (marker.tag as Building).minZoom
+        }
+    }
 }
+
+
+class Building(val name: String, val coords: LatLng, val isFavorite: Boolean=false, var minZoom: Float=14.0f)
