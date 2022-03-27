841683868033f6360e1a67ab7dc6d3161c4c2626
Nirav Adunuthula <adunuthulan@gmail.com>
2022-03-26 18:09:01 -0700

    Add User class to store profile info

---
+++ app/src/main/java/com/example/covider/models/User.kt
@@ -0,0 +1,17 @@
+package com.example.covider.models
+
+class User(var name:String, var email: String, var phoneNumber: String)
+{
+    public fun reportUserHealthStat(){
+
+    }
+
+    public fun getLisOfUserHealthStats(){
+
+    }
+
+    public fun setSchedule(){
+
+    }
+
+}
