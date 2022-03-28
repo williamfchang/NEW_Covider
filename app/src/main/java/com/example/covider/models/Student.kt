2ab64411678a22a2b41a1668db563eab0f28eb4e
Nirav Adunuthula <adunuthulan@gmail.com>
2022-03-28 01:31:56 -0700

    Updated Covider models

--- app/src/main/java/com/example/covider/models/Student.kt
+++ app/src/main/java/com/example/covider/models/Student.kt
@@ -1,5 +1,5 @@
 package com.example.covider.models
 
-class Student(name:String, email: String, uscID: String) : User(name, email, uscID){
+class Student(name:String, email: String, studentID: String) : User(name, email, studentID, CoviderEnums.UserType.STUDENT){
 
 }
---
+++ app/src/main/java/com/example/covider/models/Visit.kt
@@ -0,0 +1,5 @@
+package com.example.covider.models
+
+open class Visit(var startTime: Int = 0, var endTime: Int = 0, var buildingID: String) {
+
+}
